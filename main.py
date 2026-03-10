73515aca1d1c48c3c92d7d02da641f570a697024
salvo <salvacalo.04@gmail.com>
2026-03-10 14:58:37 +0100

    fix:    - changed pyPdf to pyPdf2    - add an option to not download pdf from source every time    - add a filter to file in folder that got analized Signed-off-by: salvo <salvacalo.04@gmail.com>

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
-from pypdf import PdfReader
+from PyPDF2 import PdfReader
 import os
 
-TARGET_URL = "https://www.ladispe.polito.it/corsi/ContrAutoInf270/"
-
-html = requests.get(TARGET_URL)
-soup = BeautifulSoup(html.text)
-a_tags = soup.find_all('a')
-
-i = 0
-for a in a_tags:
-    if a['href'].endswith(".pdf"):
-        response = requests.get(TARGET_URL + a.get('href'))
-        pdf = open("pdf"+str(i)+".pdf", 'wb')
-        pdf.write(response.content)
-        pdf.close()
-        i += 1
+download_file=input("Vuoi scaricare i dati dal sito?")
+if download_file=="yes":
+    TARGET_URL = "https://www.ladispe.polito.it/corsi/ContrAutoInf270/"
+    html = requests.get(TARGET_URL)
+    soup = BeautifulSoup(html.text)
+    a_tags = soup.find_all('a')
+    i = 0
+    for a in a_tags:
+        if a['href'].endswith(".pdf"):
+            response = requests.get(TARGET_URL + a.get('href'))
+            pdf = open("pdf"+str(i)+".pdf", 'wb')
+            pdf.write(response.content)
+            pdf.close()
+            i += 1
 
 all_files = os.listdir()
 
 word_to_search = input("Che parola vuoi cercare nei pdf? ")
 for file in all_files:
-    reader = PdfReader(file)
-    print(f"Sto cercando in {file}")
-    for i in range(len(reader.pages)):
-        page = reader.pages[i]
-        text = page.extract_text()
-        if word_to_search.lower() in text.lower():
-            print(f"Word trovata in {file}")
+    if file.endswith(".pdf") and os.path.isfile(file):
+        reader = PdfReader(file, strict=False)
+        print(f"Sto cercando in {file}")
+        for i in range(len(reader.pages)):
+            page = reader.pages[i]
+            text = page.extract_text()
+            if word_to_search.lower() in text.lower():
+                print(f"Word trovata in {file}")
